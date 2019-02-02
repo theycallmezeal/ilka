289ab5526b2cb55a9985bfa015470f8f5d896ec7
Jacob Adams <tookmund@gmail.com>
2019-02-02 00:36:47 -0500

    edit: add ilka.js and remove basic classes

--- edit/script.js
+++ edit/script.js
@@ -1,21 +1,3 @@
-class Passage {
-	constructor(speaker, text) {
-		this.speaker = speaker;
-		this.text = text;
-	}
-}
-
-class MCQ {
-	constructor(question, answers, indexOfCorrect) {
-		this.question = question;
-		this.answers = answers;
-		this.indexOfCorrect = indexOfCorrect;
-	}
-}
-
-Vue.prototype.Passage = Passage;
-Vue.prototype.MCQ = MCQ;
-
 Vue.component("edit-story", {
 	props: ["storyObject"],
 	template: `
