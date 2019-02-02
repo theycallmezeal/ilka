f3550ac032b96fac9cb81965cad08da78129e01a
Adam <zealan@gmail.com>
2019-02-01 21:54:33 -0500

    add basic classes

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
- 
+class Story {
+	constructor() {
+		this.title = "";
+		this.list = [];
+	}
+}
+
+class Passage {
+	constructor() {
+		this.speaker = "";
+		this.text = "";
+	}
+}
+
+class MCQ {
+	constructor() {
+		this.question = "";
+		this.answers = [];
+		this.indexOfCorrect = null;
+	}
+}
