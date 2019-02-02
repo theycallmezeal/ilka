94c13564df287eb33cae3f98d5cfb1d5c4796d64
Jacob Adams <tookmund@gmail.com>
2019-02-02 00:35:06 -0500

    Add basic classes to ilka.js

---
+++ ilka.js
@@ -0,0 +1,23 @@
+class Passage {
+	constructor(speaker, text) {
+		this.speaker = speaker;
+		this.text = text;
+	}
+}
+
+class MCQ {
+	constructor(question, answers, indexOfCorrect) {
+		this.question = question;
+		this.answers = answers;
+		this.indexOfCorrect = indexOfCorrect;
+	}
+}
+
+Vue.prototype.Passage = Passage;
+Vue.prototype.MCQ = MCQ;
+
+function save() {
+}
+function load() {
+
+}
