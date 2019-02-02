c3a050bd76e00705071c19e7ac8f2bdc71a9f9fb
Jacob Adams <tookmund@gmail.com>
2019-02-02 01:00:31 -0500

    Save story to json

--- ilka.js
+++ ilka.js
@@ -18,10 +18,19 @@ class MCQ {
 Vue.prototype.Passage = Passage;
 Vue.prototype.MCQ = MCQ;
 
-function save() {
 
+function downloadObjectAsJson(exportObj, exportName) {
+	var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+	var downloadAnchorNode = document.createElement('a');
+	downloadAnchorNode.setAttribute("href",		dataStr);
+	downloadAnchorNode.setAttribute("download", exportName + ".json");
+	document.body.appendChild(downloadAnchorNode); // required for firefox
+	downloadAnchorNode.click();
+	downloadAnchorNode.remove();
 }
-
+function save() {
+	downloadObjectAsJson(app.story, "story");
+}
 function load() {
 
 }
