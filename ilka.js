12ea4a1f4f6f5e70d33f42610027693d43e9cd71
Jacob Adams <tookmund@gmail.com>
2019-02-02 01:01:07 -0500

    save: cite source

--- ilka.js
+++ ilka.js
@@ -18,7 +18,7 @@ class MCQ {
 Vue.prototype.Passage = Passage;
 Vue.prototype.MCQ = MCQ;
 
-
+// https://stackoverflow.com/a/30800715
 function downloadObjectAsJson(exportObj, exportName) {
 	var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
 	var downloadAnchorNode = document.createElement('a');
