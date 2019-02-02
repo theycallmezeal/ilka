6b79f9ff7b4c7cb6c6457c61d102e7dc66b70b2d
Adam <zealan@gmail.com>
2019-02-02 11:56:16 -0500

    make more semantic templates in view mode

--- view.js
+++ view.js
@@ -2,7 +2,7 @@ Vue.component("view-story", {
 	props: ["storyObject"],
 	template: `
 		<div>
-			<p>title: {{ storyObject.title }}</p>
+			<h1>{{ storyObject.title }}</h1>
 			<div v-for="item in this.$parent.visibleItems">
 				<view-passage v-if="item.type == 'passage'" v-bind:speaker="item.speaker" v-bind:text="item.text" v-bind:translation="item.translation"></view-passage>
 				<view-mcq v-if="item.type == 'mcq'" v-bind:question="item.question" v-bind:translation="item.translation" v-bind:answers="item.answers" v-bind:answerTranslations="item.answerTranslations" v-bind:indexOfCorrect="item.indexOfCorrect"></view-mcq>
@@ -20,7 +20,7 @@ Vue.component("view-passage", {
 	},
 	template: `
 		<div class="passage">
-			<p>speaker: {{ speaker }}</p>
+			<p class="speaker">{{ speaker }}</p>
 			<p>{{ text }} <span v-on:click="toggle = !toggle">(?)</span></p>
 			<p v-if="toggle">{{ translation }}</p>
 		</div>
@@ -43,10 +43,9 @@ Vue.component("view-mcq", {
 	},
 	template: `
 		<div class="mcq">
-			<p>question: {{ question }} <span v-on:click="toggle = !toggle">(?)</span></p>
-			<p v-if="toggle">translation: {{ translation }}</p>
+			<p>{{ question }} <span v-on:click="toggle = !toggle">(?)</span></p>
+			<p v-if="toggle">{{ translation }}</p>
 			<view-mcq-answer v-for="(answer, i) in answers" v-bind:answer="answer" v-bind:answerTranslation="answerTranslations[i]" v-bind:isCorrect="i == indexOfCorrect"></view-mcq-answer>
-			<p>index of correct: {{ indexOfCorrect }}</p>
 		</div>
 	`
 });
@@ -62,10 +61,10 @@ Vue.component("view-mcq-answer", {
 	template: `
 		<div>
 			<p>{{ answer }} <span v-on:click="toggle = !toggle">(?)</span>
-					<span v-if="hasBeenSelected && isCorrect">correct!</span>
-					<span v-else-if="hasBeenSelected">wrong :(</span>
-					<button v-else-if="!hasBeenSelected && isCorrect" v-on:click="$parent.revealAll()">CLICK MEH</button>
-					<button v-else v-on:click="hasBeenSelected = true">CLICK MEH</button>
+					<span v-if="hasBeenSelected && isCorrect">&check;</span>
+					<span v-else-if="hasBeenSelected">X</span>
+					<button v-else-if="!hasBeenSelected && isCorrect" v-on:click="$parent.revealAll()">&nbsp;</button>
+					<button v-else v-on:click="hasBeenSelected = true">&nbsp;</button>
 			</p>
 			<p v-if="toggle">{{ answerTranslation }}</p>
 		</div>
