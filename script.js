84757a5d1ea164a1196316ce44dbd2d61ecbbb5c
Adam <zealan@gmail.com>
2019-02-01 23:03:35 -0500

    add components; render story with JS object as input

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+class Passage {
+	constructor(speaker, text) {
+		this.speaker = speaker;
+		this.text = text;
+	}
+}
+
+class MCQ {
+	constructor(question, answers, indexOfCorrect) {
+		this.question = question;
+		this.answers = answers;
+		this.indexOfCorrect = indexOfCorrect;
+	}
+}
+
+Vue.prototype.Passage = Passage;
+Vue.prototype.MCQ = MCQ;
+
 Vue.component("view-story", {
-	props: ["title"],
+	props: ["storyObject"],
 	template: `
 		<div>
-			<p>title: {{ title }}</p>
+			<p>title: {{ storyObject.title }}</p>
+			<div v-for="item in storyObject.items">
+				<view-passage v-if="item instanceof Passage" v-bind:speaker="item.speaker" v-bind:text="item.text"></view-passage>
+				<view-mcq v-if="item instanceof MCQ" v-bind:question="item.question" v-bind:answers="item.answers" v-bind:indexOfCorrect="item.indexOfCorrect"></view-mcq>
+			</div>
 		</div>
 	`
 });
@@ -31,5 +53,14 @@ Vue.component("view-mcq", {
 var app = new Vue({
 	el: "#app",
 	data: {
+		sampleStory: {
+			title: "beepis",
+			items: [
+				new Passage("Georg", "lol your house is on fire"),
+				new Passage("Vicky", "dude wtf"),
+				new MCQ("Whose house is on fire?", ["Georg", "Vicky"], 0),
+				new Passage("Georg", "ayy lmao")
+			]
+		},
 	}
 });
