e80e3df268d1e0cdbc029f6c3a430b6202cbe253
Adam <zealan@gmail.com>
2019-02-01 22:38:58 -0500

    add view components

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-class Story {
-	constructor() {
-		this.title = "";
-		this.list = [];
-	}
-}
+Vue.component("view-story", {
+	props: ["title"],
+	template: `
+		<div>
+			<p>title: {{ title }}</p>
+		</div>
+	`
+});
 
-class Passage {
-	constructor() {
-		this.speaker = "";
-		this.text = "";
-	}
-}
+Vue.component("view-passage", {
+	props: ["speaker", "text"],
+	template: `
+		<div>
+			<p>speaker: {{ speaker }}</p>
+			<p>{{ text }}</p>
+		</div>
+	`
+});
 
-class MCQ {
-	constructor() {
-		this.question = "";
-		this.answers = [];
-		this.indexOfCorrect = null;
+Vue.component("view-mcq", {
+	props: ["question", "answers", "indexOfCorrect"],
+	template: `
+		<div>
+			<p>question: {{ question }}</p>
+			<p v-for="(answer, i) in answers">{{ answer }} <span v-if="i == indexOfCorrect">(correct!)</span></p>
+			<p>index of correct: {{ indexOfCorrect }}</p>
+		</div>
+	`
+});
+
+var app = new Vue({
+	el: "#app",
+	data: {
 	}
-}
+});
